5c577581a414910abe3efe5043ec13a06d11cfab
Jimmy Yu <Jyu@chicagotechacademy.org>
2014-03-04 14:33:05 -0600

    first commit by rush who had to fix directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp2::Application.config.secret_token = '30333f7fb4ab91fd91d145747e363d7068104e2530a2379ba6657cee78f8358b537cd3eca3497adfa4fecb490a074b372a28fe5b70702dffb2afab015f4db83c'
